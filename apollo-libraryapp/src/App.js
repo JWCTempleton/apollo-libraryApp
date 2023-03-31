d9076ffc5b2a0dabe9ffac48309091f314781152
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-30 19:57:22 -0700

    Add react-router library, add routes for a Home page and a route for an authors page that renders the previously completed Author component

--- apollo-libraryapp/src/App.js
+++ apollo-libraryapp/src/App.js
@@ -1,19 +1,42 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import "./App.css";
 import Authors from "./components/Authors";
+import Home from "./components/Home";
 import { ALL_AUTHORS } from "./queries";
 
 function App() {
   const result = useQuery(ALL_AUTHORS);
 
+  const padding = {
+    padding: 5,
+  };
   if (result.loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="App">
-      <Authors authors={result.data.allAuthors} />
-    </div>
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/authors">
+          authors
+        </Link>
+      </div>
+      <Routes>
+        <Route
+          path="/authors"
+          element={<Authors authors={result.data.allAuthors} />}
+        />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Library app, JWCT Designs 2023</i>
+      </div>
+    </Router>
   );
 }
 
---
+++ apollo-libraryapp/src/components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Library App</h2>
+      <p>A react front end for a library app using apollo graphql</p>
+    </div>
+  );
+};
+
+export default Home;
